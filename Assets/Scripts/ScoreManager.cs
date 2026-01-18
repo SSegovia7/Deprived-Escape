6437721add4c63c96f6157f11e118443f5168ead
karlklaffen <karlklaffen@gmail.com>
2026-01-18 00:34:19 -0800

    Created score manager object

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    private int score;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddScore()
+    {
+        score++;
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+    }
+
+    
+}
