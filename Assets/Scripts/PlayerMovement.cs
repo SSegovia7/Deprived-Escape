aba05b3873f50c97eea5f98141fe74a54afcf248
karlklaffen <karlklaffen@gmail.com>
2026-01-17 19:23:35 -0800

    Updated player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,12 +3,22 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    enum Direction
+    {
+        LEFT = 0,
+        RIGHT = 1,
+        UP = 2,
+        DOWN = 3
+    }
+
     public float speed;
     private PlayerInput input;
     private InputAction moveAction;
 
     private Rigidbody2D rb;
     private SpriteRenderer rend;
+
+    private Direction lastFacingDir;
 
     private void Awake()
     {
@@ -22,18 +32,59 @@ public class PlayerMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //moveAction.ChangeBinding()
+        lastFacingDir = Direction.UP;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    private void SetFacing(bool up)
+    private void SetFacing(Direction dir)
     {
-        transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, up ? 0 : 180);
+        int newAngle = 0;
+        switch (dir)
+        {
+            case Direction.LEFT:
+                newAngle = 90;
+                break;
+            case Direction.RIGHT:
+                newAngle = 270;
+                break;
+            case Direction.UP:
+                newAngle = 0;
+                break;
+            case Direction.DOWN:
+                newAngle = 180;
+                break;
+
+        }
+        transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, newAngle);
+    }
+
+    private Direction GetMostExtremeDirection(Vector2 movementVec)
+    {
+        float leftExt = -movementVec.x;
+        float rightExt = movementVec.x;
+        float upExt = movementVec.y;
+        float downExt = -movementVec.y;
+
+        float[] dirs = { leftExt, rightExt, upExt, downExt };
+
+        int largestDirIndex = (int)lastFacingDir;
+
+        for (int i = 0; i < dirs.Length; i++)
+        {
+            if (dirs[i] > dirs[largestDirIndex])
+            {
+                largestDirIndex = i;
+                break;
+            }
+        }
+
+        return (Direction)largestDirIndex;
     }
 
     private void FixedUpdate()
@@ -44,9 +95,8 @@ public class PlayerMovement : MonoBehaviour
         //Debug.Log(movement);
         rb.AddForce(movement, ForceMode2D.Impulse);
 
-        if (inputVec.y > 0)
-            SetFacing(true);
-        else if (inputVec.y < 0)
-            SetFacing(false);
+        Direction dirToFace = GetMostExtremeDirection(movement);
+        SetFacing(dirToFace);
+        lastFacingDir = dirToFace;
     }
 }
